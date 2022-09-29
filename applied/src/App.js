536ca5b164de6b546d69388c2dd172f19f82d91a
JPhillip018 <jphillip018@gmail.com>
2022-09-29 14:50:44 -0400

    created baseURL

--- applied/src/App.js
+++ applied/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
 import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 
-import CreateJob from './components/CreateJob'
-import ShowJobList from './components/ShowJobList'
-import ShowJobDetails from './components/ShowJobDetails'
-import UpdateJobInfo from './components/UpdateJobInfo'
+// import CreateJob from './components/CreateJob'
+// import ShowJobList from './components/ShowJobList'
+// import ShowJobDetails from './components/ShowJobDetails'
+// import UpdateJobInfo from './components/UpdateJobInfo'
+
+let baseURL = ''
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000'
+} else {
+  baseURL = 'https://applied-backend-22.herokuapp.com'
+}
+
+console.log('current base URL:', baseURL)
+
 
 class App extends Component {
   constructor(props) {

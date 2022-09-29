d73a4100ddae4394d42491cc2143183eafd7bbfe
ethanvannest <ethan3vannest@gmail.com>
2022-09-28 19:02:27 -0600

    installed react router and Created routes in app.js

--- applied/src/App.js
+++ applied/src/App.js
@@ -1,4 +1,26 @@
+import { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+
+import CreateJob from './components/CreateJob'
+import ShowJobList from './components/ShowJobList'
+import ShowJobDetails from './components/ShowJobDetails'
+import UpdateJobInfo from './components/UpdateJobInfo'
+
+class App extends Component{
+  render() {
+    return (
+      <Router>
+        <Route exact path='/' component={ShowJobList} />
+        <Route exact path='/create-job' component={CreateJob}/>
+        <Route exact path='/show-job/:id' component={ShowJobDetails}/>
+        <Route exact path='/edit-job/:id' component={UpdateJobInfo}/>
+      </Router>
+    )
+  }
+}
+
 
 
 
